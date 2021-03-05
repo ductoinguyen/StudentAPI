04a73ff011f3aa1cffd79f5e5eee2eca33729965
ductoinguyen <55026343+ductoinguyen@users.noreply.github.com>
2021-03-05 19:12:41 +0700

    update SV

--- app.py
+++ app.py
@@ -43,6 +43,357 @@ students = [{
     {"name": "AKD", "phone": "0572832832"},
     {"name": "GSH", "phone": "0768767732"},
   ]
+}, {
+  "id": "18021292",
+  "name": "Nguyễn Đức Tới",
+  "birthday": "2000-09-08",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021292@vnu.edu.vn",
+    "otheremail": "18021292@gmail.com"
+  },
+  "contacts": [
+    {"name": "ABC", "phone": "0989898989"},
+    {"name": "XYZ", "phone": "0639273682"},
+  ]
+}, {
+  "id": "18021318",
+  "name": "Vũ Thành Trung",
+  "birthday": "2000-09-04",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021318@vnu.edu.vn",
+    "otheremail": "18021318@gmail.com"
+  },
+  "contacts": [
+    {"name": "NDJ", "phone": "0567826323"},
+    {"name": "KLM", "phone": "0456782672"},
+  ]
+}, {
+  "id": "18020419",
+  "name": "Nguyễn Hùng Duy",
+  "birthday": "2000-05-29",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18020419@vnu.edu.vn",
+    "otheremail": "18020419@gmail.com"
+  },
+  "contacts": [
+    {"name": "AKD", "phone": "0572832832"},
+    {"name": "GSH", "phone": "0768767732"},
+  ]
+}, {
+  "id": "18021292",
+  "name": "Nguyễn Đức Tới",
+  "birthday": "2000-09-08",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021292@vnu.edu.vn",
+    "otheremail": "18021292@gmail.com"
+  },
+  "contacts": [
+    {"name": "ABC", "phone": "0989898989"},
+    {"name": "XYZ", "phone": "0639273682"},
+  ]
+}, {
+  "id": "18021318",
+  "name": "Vũ Thành Trung",
+  "birthday": "2000-09-04",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021318@vnu.edu.vn",
+    "otheremail": "18021318@gmail.com"
+  },
+  "contacts": [
+    {"name": "NDJ", "phone": "0567826323"},
+    {"name": "KLM", "phone": "0456782672"},
+  ]
+}, {
+  "id": "18020419",
+  "name": "Nguyễn Hùng Duy",
+  "birthday": "2000-05-29",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18020419@vnu.edu.vn",
+    "otheremail": "18020419@gmail.com"
+  },
+  "contacts": [
+    {"name": "AKD", "phone": "0572832832"},
+    {"name": "GSH", "phone": "0768767732"},
+  ]
+}, {
+  "id": "18021292",
+  "name": "Nguyễn Đức Tới",
+  "birthday": "2000-09-08",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021292@vnu.edu.vn",
+    "otheremail": "18021292@gmail.com"
+  },
+  "contacts": [
+    {"name": "ABC", "phone": "0989898989"},
+    {"name": "XYZ", "phone": "0639273682"},
+  ]
+}, {
+  "id": "18021318",
+  "name": "Vũ Thành Trung",
+  "birthday": "2000-09-04",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021318@vnu.edu.vn",
+    "otheremail": "18021318@gmail.com"
+  },
+  "contacts": [
+    {"name": "NDJ", "phone": "0567826323"},
+    {"name": "KLM", "phone": "0456782672"},
+  ]
+}, {
+  "id": "18020419",
+  "name": "Nguyễn Hùng Duy",
+  "birthday": "2000-05-29",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18020419@vnu.edu.vn",
+    "otheremail": "18020419@gmail.com"
+  },
+  "contacts": [
+    {"name": "AKD", "phone": "0572832832"},
+    {"name": "GSH", "phone": "0768767732"},
+  ]
+}, {
+  "id": "18021292",
+  "name": "Nguyễn Đức Tới",
+  "birthday": "2000-09-08",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021292@vnu.edu.vn",
+    "otheremail": "18021292@gmail.com"
+  },
+  "contacts": [
+    {"name": "ABC", "phone": "0989898989"},
+    {"name": "XYZ", "phone": "0639273682"},
+  ]
+}, {
+  "id": "18021318",
+  "name": "Vũ Thành Trung",
+  "birthday": "2000-09-04",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021318@vnu.edu.vn",
+    "otheremail": "18021318@gmail.com"
+  },
+  "contacts": [
+    {"name": "NDJ", "phone": "0567826323"},
+    {"name": "KLM", "phone": "0456782672"},
+  ]
+}, {
+  "id": "18020419",
+  "name": "Nguyễn Hùng Duy",
+  "birthday": "2000-05-29",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18020419@vnu.edu.vn",
+    "otheremail": "18020419@gmail.com"
+  },
+  "contacts": [
+    {"name": "AKD", "phone": "0572832832"},
+    {"name": "GSH", "phone": "0768767732"},
+  ]
+}, {
+  "id": "18021292",
+  "name": "Nguyễn Đức Tới",
+  "birthday": "2000-09-08",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021292@vnu.edu.vn",
+    "otheremail": "18021292@gmail.com"
+  },
+  "contacts": [
+    {"name": "ABC", "phone": "0989898989"},
+    {"name": "XYZ", "phone": "0639273682"},
+  ]
+}, {
+  "id": "18021318",
+  "name": "Vũ Thành Trung",
+  "birthday": "2000-09-04",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021318@vnu.edu.vn",
+    "otheremail": "18021318@gmail.com"
+  },
+  "contacts": [
+    {"name": "NDJ", "phone": "0567826323"},
+    {"name": "KLM", "phone": "0456782672"},
+  ]
+}, {
+  "id": "18020419",
+  "name": "Nguyễn Hùng Duy",
+  "birthday": "2000-05-29",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18020419@vnu.edu.vn",
+    "otheremail": "18020419@gmail.com"
+  },
+  "contacts": [
+    {"name": "AKD", "phone": "0572832832"},
+    {"name": "GSH", "phone": "0768767732"},
+  ]
+}, {
+  "id": "18021292",
+  "name": "Nguyễn Đức Tới",
+  "birthday": "2000-09-08",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021292@vnu.edu.vn",
+    "otheremail": "18021292@gmail.com"
+  },
+  "contacts": [
+    {"name": "ABC", "phone": "0989898989"},
+    {"name": "XYZ", "phone": "0639273682"},
+  ]
+}, {
+  "id": "18021318",
+  "name": "Vũ Thành Trung",
+  "birthday": "2000-09-04",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021318@vnu.edu.vn",
+    "otheremail": "18021318@gmail.com"
+  },
+  "contacts": [
+    {"name": "NDJ", "phone": "0567826323"},
+    {"name": "KLM", "phone": "0456782672"},
+  ]
+}, {
+  "id": "18020419",
+  "name": "Nguyễn Hùng Duy",
+  "birthday": "2000-05-29",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18020419@vnu.edu.vn",
+    "otheremail": "18020419@gmail.com"
+  },
+  "contacts": [
+    {"name": "AKD", "phone": "0572832832"},
+    {"name": "GSH", "phone": "0768767732"},
+  ]
+}, {
+  "id": "18021292",
+  "name": "Nguyễn Đức Tới",
+  "birthday": "2000-09-08",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021292@vnu.edu.vn",
+    "otheremail": "18021292@gmail.com"
+  },
+  "contacts": [
+    {"name": "ABC", "phone": "0989898989"},
+    {"name": "XYZ", "phone": "0639273682"},
+  ]
+}, {
+  "id": "18021318",
+  "name": "Vũ Thành Trung",
+  "birthday": "2000-09-04",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021318@vnu.edu.vn",
+    "otheremail": "18021318@gmail.com"
+  },
+  "contacts": [
+    {"name": "NDJ", "phone": "0567826323"},
+    {"name": "KLM", "phone": "0456782672"},
+  ]
+}, {
+  "id": "18020419",
+  "name": "Nguyễn Hùng Duy",
+  "birthday": "2000-05-29",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18020419@vnu.edu.vn",
+    "otheremail": "18020419@gmail.com"
+  },
+  "contacts": [
+    {"name": "AKD", "phone": "0572832832"},
+    {"name": "GSH", "phone": "0768767732"},
+  ]
+}, {
+  "id": "18021292",
+  "name": "Nguyễn Đức Tới",
+  "birthday": "2000-09-08",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021292@vnu.edu.vn",
+    "otheremail": "18021292@gmail.com"
+  },
+  "contacts": [
+    {"name": "ABC", "phone": "0989898989"},
+    {"name": "XYZ", "phone": "0639273682"},
+  ]
+}, {
+  "id": "18021318",
+  "name": "Vũ Thành Trung",
+  "birthday": "2000-09-04",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021318@vnu.edu.vn",
+    "otheremail": "18021318@gmail.com"
+  },
+  "contacts": [
+    {"name": "NDJ", "phone": "0567826323"},
+    {"name": "KLM", "phone": "0456782672"},
+  ]
+}, {
+  "id": "18020419",
+  "name": "Nguyễn Hùng Duy",
+  "birthday": "2000-05-29",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18020419@vnu.edu.vn",
+    "otheremail": "18020419@gmail.com"
+  },
+  "contacts": [
+    {"name": "AKD", "phone": "0572832832"},
+    {"name": "GSH", "phone": "0768767732"},
+  ]
+}, {
+  "id": "18021292",
+  "name": "Nguyễn Đức Tới",
+  "birthday": "2000-09-08",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021292@vnu.edu.vn",
+    "otheremail": "18021292@gmail.com"
+  },
+  "contacts": [
+    {"name": "ABC", "phone": "0989898989"},
+    {"name": "XYZ", "phone": "0639273682"},
+  ]
+}, {
+  "id": "18021318",
+  "name": "Vũ Thành Trung",
+  "birthday": "2000-09-04",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18021318@vnu.edu.vn",
+    "otheremail": "18021318@gmail.com"
+  },
+  "contacts": [
+    {"name": "NDJ", "phone": "0567826323"},
+    {"name": "KLM", "phone": "0456782672"},
+  ]
+}, {
+  "id": "18020419",
+  "name": "Nguyễn Hùng Duy",
+  "birthday": "2000-05-29",
+  "gender": "Nam",
+  "emails": {
+    "vnuemai": "18020419@vnu.edu.vn",
+    "otheremail": "18020419@gmail.com"
+  },
+  "contacts": [
+    {"name": "AKD", "phone": "0572832832"},
+    {"name": "GSH", "phone": "0768767732"},
+  ]
 }]
 
 app = Flask(__name__)
